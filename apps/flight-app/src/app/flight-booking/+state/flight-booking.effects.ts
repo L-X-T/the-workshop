35d3ce7e0dd157d44b5d982bbb89ca66fa9d27ec
Alex Thalhammer <alex@angular-software.at>
2021-05-24 18:43:05 +0200

    [ngrx] Setup the feature module store

---
+++ apps/flight-app/src/app/flight-booking/+state/flight-booking.effects.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class FlightBookingEffects {
+  /*loadFlightBookings$ = createEffect(() => {
+    return this.actions$.pipe(
+
+      ofType(FlightBookingActions.loadFlightBookings),
+      /!** An EMPTY observable only emits completion. Replace with your own observable API request *!/
+      concatMap(() => EMPTY)
+    );
+  });
+
+
+  constructor(private actions$: Actions) {}*/
+}
