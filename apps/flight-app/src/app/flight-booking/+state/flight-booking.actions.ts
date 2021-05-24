0e224e3af2dc110bdf4950a198d6a7e07e727829
Alex Thalhammer <alex@angular-software.at>
2021-05-24 19:39:43 +0200

    [ngrx] Bonus: Error Handling

--- apps/flight-app/src/app/flight-booking/+state/flight-booking.actions.ts
+++ apps/flight-app/src/app/flight-booking/+state/flight-booking.actions.ts
@@ -5,4 +5,6 @@ export const loadFlightBookings = createAction('[FlightBooking] Load FlightBooki
 
 export const loadFlights = createAction('[FlightBooking] Load Flights', props<{ from: string; to: string; urgent: boolean }>());
 
+export const loadFlightsError = createAction('[FlightBooking] Load Flights Error');
+
 export const updateFlight = createAction('[FlightBooking] Update Flight', props<{ flight: Flight }>());
--- apps/flight-app/src/app/flight-booking/+state/flight-booking.effects.ts
+++ apps/flight-app/src/app/flight-booking/+state/flight-booking.effects.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { map, switchMap } from 'rxjs/operators';
+import { catchError, map, switchMap } from 'rxjs/operators';
 
 import { FlightService } from '@flight-workspace/flight-lib';
 
-import { loadFlights, loadFlightBookings } from './flight-booking.actions';
+import { loadFlights, loadFlightBookings, loadFlightsError } from './flight-booking.actions';
+import { of } from 'rxjs';
 
 @Injectable()
 export class FlightBookingEffects {
-  loadFlights$ = createEffect(() =>
+  loadFlights$ = createEffect((): any =>
     this.actions$.pipe(
       ofType(loadFlights),
-      switchMap((a) => this.flightService.find(a.from, a.to, a.urgent)),
-      map((flights) => loadFlightBookings({ flights }))
+      switchMap((a) =>
+        this.flightService.find(a.from, a.to, a.urgent).pipe(
+          map((flights) => loadFlightBookings({ flights })),
+          catchError((err) => of(loadFlightsError()))
+        )
+      )
     )
   );
 
