76983504d627c0eab388e752cadb2d0e55506d8b
Alex Thalhammer <alex@angular-software.at>
2021-05-24 13:54:15 +0200

    [MF] Step 4: Switch to Dynamic Federation

--- module-federation/projects/shell/src/app/app.routes.ts
+++ module-federation/projects/shell/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
 export const APP_ROUTES: Routes = [
   {
@@ -10,10 +11,18 @@ export const APP_ROUTES: Routes = [
   },
 
   // Your route here:
-
-  {
+  /*{
     path: 'flights',
     loadChildren: () => import('mfe1/Module').then((m) => m.FlightsModule)
+  },*/
+  {
+    path: 'flights',
+    loadChildren: () =>
+      loadRemoteModule({
+        // remoteEntry: 'http://localhost:3000/remoteEntry.js',
+        remoteName: 'mfe1',
+        exposedModule: './Module'
+      }).then((m) => m.FlightsModule)
   },
 
   {
