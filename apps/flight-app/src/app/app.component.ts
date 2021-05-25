d9bd20862e54c9a7676103adfe769c56bfaea5d3
Alex Thalhammer <alex@angular-software.at>
2021-05-25 19:07:25 +0200

    [resolver] Add loading indicator

--- apps/flight-app/src/app/app.component.ts
+++ apps/flight-app/src/app/app.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { NavigationCancel, NavigationError, ResolveEnd, ResolveStart, Router, RouterEvent } from '@angular/router';
 
 @Component({
   selector: 'flight-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  isResolving = true;
+
+  constructor(private router: Router) {
+    router.events.subscribe((routerEvent: RouterEvent) => {
+      this.checkRouterEvent(routerEvent);
+    });
+  }
+
+  checkRouterEvent(routerEvent: RouterEvent): void {
+    if (routerEvent instanceof ResolveStart) {
+      this.isResolving = true;
+    }
+
+    if (routerEvent instanceof ResolveEnd || routerEvent instanceof NavigationCancel || routerEvent instanceof NavigationError) {
+      this.isResolving = false;
+    }
+  }
+}
